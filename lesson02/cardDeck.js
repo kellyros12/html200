47d33574068011179db9865b44ebeac4523abb82
Kelly Ros <kellyros91@gmail.com>
2021-09-20 23:03:00 -0700

    completed challenge 5

---
+++ lesson02/cardDeck.js
@@ -0,0 +1,8 @@
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+
+for (const suit of suits){
+  for (const card of cards){
+   console.log(card + ' of ' + suit);
+  }
+}
