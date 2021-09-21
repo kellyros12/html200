718aba472d21431b257bdad46fd2d677eab60942
Kelly Ros <kellyros91@gmail.com>
2021-09-20 23:08:18 -0700

    reverted update

--- lesson02/cardDeck.js
+++ lesson02/cardDeck.js
@@ -4,5 +4,5 @@ const cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Que
 for (const suit of suits){
   for (const card of cards){
    console.log(card + ' of ' + suit);
-};
+  }
 }
