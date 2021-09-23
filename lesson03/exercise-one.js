f557b3fe54ad03babe074d27fb14c1dffd4efcb4
Kelly Ros <kellyros91@gmail.com>
2021-09-22 23:38:25 -0700

    Completed challenge 1

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,13 @@
+let arr = ['This', 'is', 'an', 'array', 'challenge'];
+
+console.log(arr);
+
+arr.push('added');
+
+console.log(arr);
+
+arr.splice(2, 1);
+
+console.log(arr);
+
+console.log(arr.join(','));
